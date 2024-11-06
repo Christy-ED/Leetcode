433cb12b6455917b3b6d7077b93fad1918f2868b
ChristyED <147757638+Christy-ED@users.noreply.github.com>
2024-11-05 20:14:40 -0500

    Time: 0 ms (100.00%), Space: 52.6 MB (41.32%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -5,22 +5,22 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var deleteDuplicates = function(head) {
-    // create a pointer 
-    current = head;
-    while( current !== null && current.next !== null){
-        if( current.val === current.next.val) {
-            current.next = current.next.next;
-        }else{
-            current = current.next;
-        }
-    }
-    return head;
-};              
+// /**
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+// var deleteDuplicates = function(head) {
+//     // create a pointer 
+//     current = head;
+//     while( current !== null && current.next !== null){
+//         if( current.val === current.next.val) {
+//             current.next = current.next.next;
+//         }else{
+//             current = current.next;  //It does continue checking the rest of the list recursively.
+//         }
+//     }
+//     return head;
+// };              
 
 
 /**
@@ -30,24 +30,30 @@ var deleteDuplicates = function(head) {
  *     this.next = (next===undefined ? null : next)
  * }
  */
+ /**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 
-// var deleteDuplicates = function(head) {
-//     // Base case: if the list is empty or has only one node, return head
-//     if (head === null || head.next === null) {
-//         return head;
-//     }
+var deleteDuplicates = function(head) {
+    // Base case: if the list is empty or has only one node, return head
+    if (head === null || head.next === null) {
+        return head;
+    }
 
-//     // Recursive case:
-//     // If the current node and the next node have the same value
-//     if (head.val === head.next.val) {
-//         // Skip the next node by pointing to head.next.next
-//         head.next = deleteDuplicates(head.next.next);
-//     } else {
-//         // Otherwise, proceed to the next node
-//         head.next = deleteDuplicates(head.next);
-//     }
+    // Recursive case:
+    if (head.val === head.next.val) {
+        // Move to the next distinct value
+       
+        while (head.next !== null && head.next.val === head.val) {
+            head.next = head.next.next;
+        }
+        head.next = deleteDuplicates(head.next); // Skip duplicates and continue
+    } else {
+        // Otherwise, proceed to the next node
+        head.next = deleteDuplicates(head.next); //It does actually call the function deleteDuplicates to move down the list.
+    }
 
-//     return head;
-// };
-
+    return head;
+};
 
